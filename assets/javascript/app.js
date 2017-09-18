cad52de3df4622b7fde67d80b949d92a65b5e7d7
Ryan Franco <rfranco92@gmail.com>
2017-09-18 13:31:56 -0400

    Some Basic HTML and Jscript

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+$("#abbreviation").on("click", function(event) {
+	event.preventDefault();
+    $("#senators-view").empty();
+	var abbr = $("#state").val().trim();
+
+	$.ajax({
+         url: "https://api.propublica.org/congress/v1/members/senate/"+ abbr +"/current.json",
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('X-API-Key', 'pOeVTMP0uUXsG3Gn6uAQ9AT2claobUTPM0Wse53Q');},
+         success: function(response){
+            console.log(response);
+            console.log()
+            for (var i = 0; i < response.results.length; i++){
+               var specialbutton = $("<button>");
+               specialbutton.addClass("senators");
+
+               specialbutton.attr("data-name", response.results[i].name);
+               specialbutton.attr("data-api", response.results[i].api_uri);
+
+               specialbutton.text(response.results[i].name);
+
+               $("#senators-view").append(specialbutton);
+            }
+         }
+      });
+});
+
+function displaySenatorInfo(){
+   var url=$(this).attr("data-api")
+
+   $.ajax({
+         url: url,
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('X-API-Key', 'pOeVTMP0uUXsG3Gn6uAQ9AT2claobUTPM0Wse53Q');},
+         success: function(response){
+            console.log(response);
+            console.log(response.results[0].url);
+            console.log(response.results[0].twitter_account);
+            console.log(response.results[0].times_topics_url);
+
+
+         }
+   });
+}
+
+$(document).on("click", ".senators", displaySenatorInfo);
