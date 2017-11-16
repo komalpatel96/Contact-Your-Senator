239285ad2cd3f16caec4db885e2cc1addaa1dbad
Splimis <splimis@gmail.com>
2017-11-16 14:48:23 -0500

    npm stuff

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var methodOverride = require("method-override");
 
 var app = express(); 
 
@@ -14,11 +13,6 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // Serve static content for the app from the 'public' directory
 app.use("/public", express.static(__dirname + '/public'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
-
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
 
 // require('./App/routing/apiRoutes.js')(app); 
 require('./routes/htmlRoutes.js')(app);
